7177e9dcc5335cea77311f8bd985c1ea04f6acbb
Kenneth Daily <kenneth.daily@sagebase.org>
2014-11-07 11:40:27 -0800

    Added separate function to fixRows.
    Updated query2df to better handle list types.

--- synapseHelpers.py
+++ synapseHelpers.py
@@ -5,10 +5,33 @@ import sys
 import fileReader
 import numpy as np
 
-syn=synapseclient.Synapse()
-syn.login()
+# syn=synapseclient.Synapse()
+# syn.login()
+import logging
 
-def thisCodeInSynapse(parentId, file=None, description=''):
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+handler = logging.StreamHandler()
+handler.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
+def fixAnnotationRow(row, delimiter=","):
+    """Merge column values in Synapse annotations.
+    
+    """
+    
+    for k, v in row.iteritems():
+        if type(v) == list:
+            row[k] = delimiter.join(v)
+            
+    return row
+
+def fixRow(row):
+    return fixAnnotationRow(row)
+
+def thisCodeInSynapse(parentId, syn, file=None, description=''):
     """Determines the name of the file that the code is called from
     and uploads that to Synapse returning the synapseId of created codeObject.
     """
@@ -20,28 +43,75 @@ def thisCodeInSynapse(parentId, file=None, description=''):
     codeEntity = syn.store(code)
     return codeEntity
 
-def query2df(queryContent, filterSynapseFields=True):
+_syn_obj_types = ['entity', 'file', 'link', 'directory']
+
+_syn_obj_keys = ['benefactorId', 'nodeType', 'concreteType',
+                'createdByPrincipalId', 'createdOn',
+                'createdByPrincipalId', 'eTag', 'id',
+                'modifiedOn', 'modifiedByPrincipalId',
+                'noteType', 'versionLabel', 'versionComment',
+                'versionNumber', 'parentId', 'description']
+
+_entity_keys = ["%s.%s" % (x, y) for x in _syn_obj_types for y in _syn_obj_keys]
+
+def query2df(queryContent, filterSynapseFields=True, delimiter=None):
     """Converts the returned query object from Synapse into a Pandas DataFrame
-    
+
     Arguments:
     - `queryContent`: content returned from query
     - `filterSynapseFields`: Removes Synapse properties of the entity returned with "select * ..."
+    - `delimiter`: if None, takes the first item of list-type content; otherwise joins the data based on delimiter
+    
     """
+
     import pandas as pd
-    queryContent = pd.DataFrame(queryContent['results'])
-    #Remove the unecessary lists and 'entity' in names  (this should be fixed on Synapse!)
-    for key in queryContent.keys():
-        if filterSynapseFields and key in ['entity.benefactorId', 'entity.nodeType', 'entity.concreteType', 'entity.createdByPrincipalId', 'entity.createdOn', 'entity.createdByPrincipalId', 'entity.eTag', 'entity.id', 'entity.modifiedOn', 'entity.modifiedByPrincipalId', 'entity.noteType', 'entity.versionLabel', 'entity.versionComment', 'entity.versionNumber', 'entity.parentId', 'entity.description']:
-            del queryContent[key]
+
+    if type(queryContent) is dict:
+        queryContent = queryContent['results']
+
+    
+    queryContentDf = pd.DataFrame(queryContent)
+    
+    #Remove the unecessary lists and 'entity' in names (this should be fixed on Synapse!)
+    for key in queryContentDf.keys():
+        
+        if filterSynapseFields and key in _entity_keys:
+            del queryContentDf[key]
             continue
-        newkey=key.replace('entity.', '')
-        queryContent[newkey] = pd.Series([item[0] for item in queryContent[key] if type(item) is list])
-        del queryContent[key]
-    return queryContent
+
+        newkey = key
+
+        # Clean up the column names
+        for keytype in _syn_obj_types:
+            # logger.debug("Keytype: %s, key: %s" % (keytype, key))
+            if key.startswith(keytype):
+                newkey = key.replace("%s." % keytype, "")
+                break
+                
+        queryContentDf[newkey] = queryContentDf[key]
+
+        # Clean up the data
+        newseries = []
+        
+        # Iterate over cells of a column
+        for cell in queryContentDf[key]:
+            if type(cell) is list:
+                if delimiter:
+                    newseries.append(delimiter.join(cell))
+                else:
+                        newseries.append(cell[0])
+            else:
+                newseries.append(cell)
+
+        queryContentDf[newkey] = pd.Series(newseries)
+        
+        del queryContentDf[key]
+        
+    return queryContentDf
 
 
 
-def createEvaluationBoard(name, parentId, markdown='',status='OPEN', contentSource=''):
+def createEvaluationBoard(name, syn, parentId, markdown='',status='OPEN', contentSource=''):
         """Creates an evaluation where users can submit entities for evaluation by a monitoring process.
         Currently also creates a folder where the results are displayed in a leaderboard.
         
@@ -65,7 +135,7 @@ def createEvaluationBoard(name, parentId, markdown='',status='OPEN', contentSour
         return evaluation, homeWikiPage
         
 
-def updateLeaderboard(leaderboard, evaluation):
+def updateLeaderboard(syn, leaderboard, evaluation):
     """Goes through all submissions in an evalution and updates the description markdown in entity.
     
     Arguments:
